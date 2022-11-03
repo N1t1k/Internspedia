db9143136ebb0aa476069a34540bb9d781b05b80
N1t1k <115040161+N1t1k@users.noreply.github.com>
2022-11-03 21:39:34 +0530

    Add files via upload

---
+++ GUESS.java
@@ -0,0 +1,70 @@
+import java.util.*;
+class Game{
+    public int x;
+    public int a;
+    int noOfGuesses=0;
+    int Game(){
+        
+        Random rand =new Random();
+        x = rand.nextInt(100);
+        return 0;
+    }
+    void takeUserInput(){
+            
+            System.out.println("Enter your Guessed number");
+            Scanner sc = new Scanner(System.in);
+            a = sc.nextInt();
+    }
+    boolean isCorrectNumber(){
+        int count=0;
+        
+        noOfGuesses +=1;
+        if(a==x){
+            
+            System.out.format("Correct Answer, You did it in %d attempt",noOfGuesses);
+            return true;
+        }
+        else if(a>x){
+            if(noOfGuesses>=7){
+                System.out.println("The Guessed number is higher than the actual number.");
+                System.out.println("");
+                System.out.println("K trials exhausted");
+                return true;
+            }
+        System.out.println("The Guessed number is higher than the actual number.");
+        System.out.println("");
+        }
+        else if(a<x){
+            if(noOfGuesses>=7){
+                System.out.println("The Guessed number is Lower than the actual number.");
+                System.out.println("");
+                System.out.println("K trials exhausted");
+                return true;
+            }
+        System.out.println("The Guessed number is Lower than the actual number.");
+        System.out.println("");
+        }
+        
+        return false;
+    }
+        
+    
+}
+class GUESS
+{
+	public static void main(String[] args) {
+		System.out.println("****************************************************************************************************************");
+		System.out.println("                               WELCOME TO GUESSING NUMBER GAME");
+		System.out.println("****************************************************************************************************************");
+		System.out.println();
+		System.out.println();
+		Game g=new Game();
+		boolean b=false;
+		while(!b){
+		    g.takeUserInput();
+		    b=g.isCorrectNumber();
+		   
+		}
+		
+	}
+}
